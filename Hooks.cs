3ed1d2018124289f999bfd2dabb4af9c3ac710c4
HemaMurugaboopathy <h.prabhaharan@gmail.com>
2024-01-25 22:31:51 +1300

    Hooks.cs

---
+++ Hooks.cs
@@ -0,0 +1,25 @@
+﻿using Mars.Utilities;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.DevTools.V118.Browser;
+using TechTalk.SpecFlow;
+
+namespace Mars
+{
+    [Binding]
+    public sealed class Hooks : CommonDriver
+    {
+
+        [BeforeScenario]
+        public void BeforeScenarioWithTag()
+        {
+            Initialize();
+        }
+
+        [AfterScenario]
+        public void AfterScenario()
+        {
+            Close();
+        }
+    }
+
+}
